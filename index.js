92b97b755afac1fa3b5fc20bdbb53b5034bff11c
Annette Kariuki <kariukian1@gmail.com>
2023-07-05 12:55:11 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER);
+
+let num3 = 20;
+let num4 = 8;
+let mod = num3 % num4;
+
+let max = Math.max(10, 5, 20);
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
+
